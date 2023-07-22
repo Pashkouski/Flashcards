bfcd2d72372a5fad534a03da309f4e4439d8ba8a
pashkouski <satiym@gmail.com>
2023-07-22 20:28:32 +0300

    "feat: add styles"

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 import { StrictMode } from 'react'
+import './styles/index.scss'
 
 import { createRoot } from 'react-dom/client'
 
