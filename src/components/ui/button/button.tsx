785596e5c8162c3af0e08aec9ba3bc01406ac735
pashkouski <satiym@gmail.com>
2023-07-25 20:16:28 +0300

    "add component 'typography'"

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -13,6 +13,7 @@ export const Button = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+  const aasd = 5
 
   return (
     <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
