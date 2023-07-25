4b60c31108f4c903d86108379caaab1dcfc5a3e6
pashkouski <satiym@gmail.com>
2023-07-25 20:28:55 +0300

    "add component 'typography merge'"

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -13,7 +13,6 @@ export const Button = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
-  const aasd = 5
 
   return (
     <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
