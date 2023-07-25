978e81e28c3ca3ecd0dbf7cb34087a96b7f59506
pashkouski <satiym@gmail.com>
2023-07-25 19:38:08 +0300

    "add component 'typography'"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Typography } from './components/ui'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      Hello
+      <Typography variant={'body2'}>Lorem ipsum dolor sit amet.</Typography>
+    </div>
+  )
 }
