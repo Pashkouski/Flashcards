6b7a154deda25926d9a1984b7f4c9a3e6c4733a3
pashkouski <satiym@gmail.com>
2023-07-25 20:20:44 +0300

    "add component 'typography stories'"

--- src/components/ui/typography/typography.tsx
+++ src/components/ui/typography/typography.tsx
@@ -29,7 +29,5 @@ export function Typography<T extends ElementType = 'p'>(
 
   const classNames = `${s.text} ${s[variant]} ${className}`
 
-  const f = 5
-
   return <Component className={classNames} {...rest} />
 }
