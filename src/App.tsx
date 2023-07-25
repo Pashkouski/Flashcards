23e0670acbd0e1903f01510ec2fc1039517a9ac2
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-07-25 20:36:51 +0300

    train55555

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { Typography } from './components/ui'
-
 export function App() {
   return (
     <div>
-
+      <div>55555</div>
       <div>2222</div>
       <div>Hello</div>
-
     </div>
   )
 }
