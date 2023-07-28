71a28634b3c8c08a05f908a76b326c4f02feabe6
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-07-28 09:16:11 +0300

    TabPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 import { useState } from 'react'
 
-import { SliderBar, TextField } from './components/ui'
+import { itemTabType, SliderBar, TabPanel, TextField } from './components/ui'
 
 export function App() {
+  //1
   const [valueInput, setValueInput] = useState('')
 
   console.log(valueInput, 'valueInput')
   const handlerSendInputValue = (valueInput: string) => {
     alert(valueInput)
   }
+  //2
   //SIDER
   const startArrayValue = [1, 100]
   const handlerOnValueCommit = (value: number[]) => {
     alert(`Вы поставили левый ползунок на ${value[0]}  а правый на ${value[1]}`)
   }
+  //3
+  //TABPANEL
+
+  const [active, setActive] = useState('My')
+
+  const handlerTabPanel1 = (name: string) => {
+    alert(`Мне показалось или вы нажали кнопку ${name}...Перепроверьте лучше!`)
+    setActive(name)
+  }
+  const handlerTabPanel2 = (name: string) => {
+    alert(`Мне показалось или вы нажали кнопку ${name}...Перепроверьте лучше!`)
+    setActive(name)
+  }
+  const handlerTabPanel3 = (name: string) => {
+    alert(`Мне показалось или вы нажали кнопку ${name}...Перепроверьте лучше!`)
+    setActive(name)
+  }
+  const dataTabPanel: itemTabType[] = [
+    { id: 'tab1', name: 'My', onClick: handlerTabPanel1, disabled: false },
+    { id: 'tab2', name: 'All', onClick: handlerTabPanel2, disabled: false },
+    { id: 'tab3', name: 'Трейтья', onClick: handlerTabPanel3, disabled: false },
+  ]
 
   return (
     <div>
-      <div style={{ padding: '1rem' }}>
+      <div>
         <div>
           <TextField
             handlerOnChange={handlerSendInputValue}
@@ -49,12 +73,17 @@ export function App() {
         </div>
       </div>
       {/* Slider*/}
-      <div style={{ padding: '15 rem' }}>
+      <div>
         <SliderBar onValueCommit={handlerOnValueCommit} startArrayValue={startArrayValue} />
+      </div>
+      <div>
+        {/* TabPanel*/}
+        <TabPanel active={active} data={dataTabPanel} title="Title" />
       </div>
     </div>
   )
 }
+
 /*
 export function App() {
   return (
