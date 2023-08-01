b62bad4f681fad2b8353b6effe66ad68e92b2f1a
juraantoniv <juraantoniv@gmail.com>
2023-08-01 22:15:37 +0300

    add avatar component

--- src/components/ui/header/header.stories.tsx
+++ src/components/ui/header/header.stories.tsx
@@ -1,6 +1,9 @@
 import { action } from '@storybook/addon-actions'
 import { Meta, StoryObj } from '@storybook/react'
 
+import { AvatarDemo } from '../avatar'
+import { Button } from '../button'
+
 import { Header } from './header.tsx'
 
 const meta = {
@@ -14,8 +17,22 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+export const WithAvatar: Story = {
+  args: {
+    children: AvatarDemo(),
+  },
+}
+
+const button = () => {
+  return (
+    <Button variant={'primary'} style={{ marginLeft: 1500 }}>
+      Sign in
+    </Button>
+  )
+}
+
 export const WithButton: Story = {
   args: {
-    children: 'div',
+    children: button(),
   },
 }
--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, FC, ReactNode } from 'react'
+import { ComponentProps, FC, ReactElement, ReactNode } from 'react'
 
 import { clsx } from 'clsx'
 
@@ -6,7 +6,7 @@ import s from './header.module.scss'
 
 export type HeaderProps = {
   children: ReactNode
-} & ComponentProps<'header'>
+} & Omit<ComponentProps<'header'>, 'children'>
 
 export const Header: FC<HeaderProps> = ({ children, className, ...rest }) => {
   const classNames = {
